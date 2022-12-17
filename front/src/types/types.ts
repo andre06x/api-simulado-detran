6d0a99b7b55fdbf632db814f0783721b3cd2eb43
andre06x <andre06x@gmail.com>
2022-12-17 15:43:39 -0300

    exportando types

---
+++ front/src/types/types.ts
@@ -0,0 +1,20 @@
+type TypePergunta = {
+	titulo: string;
+	imagem_url: string;
+	respostas: Array<any>;
+};
+
+type TypeAlternativas = {
+	nome_alternativa: string;
+	id: string;
+	pergunta_id: string;
+	correta: boolean;
+};
+
+type TypeRespostasSalvas = {
+	pergunta_id: string;
+	alternativa_id: string;
+	alternativa_marcada: string;
+};
+
+export { TypeAlternativas, TypePergunta, TypeRespostasSalvas };
