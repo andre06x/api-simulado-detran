7489d11f55eda68d6334d8ed813558334e22a8fb
andre06x <andre06x@gmail.com>
2022-12-17 15:44:28 -0300

    criando hook de status

---
+++ front/src/hooks/useStatusResultado.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { usePerguntas, useRespostasSalvas } from '../zustand';
+
+const useStatusResultado = () => {
+	const [status, setStatus] = useState({ rc: 0, re: 0, total: 0 });
+	const respostasSalvas = useRespostasSalvas((state) => state.respostasSalvas);
+	const data = usePerguntas((state) => state.dataPerguntas);
+
+	useEffect(() => {
+		if (data !== null) {
+			let rc = 0,
+				re = 0,
+				total = 0;
+
+			for (let i = 0; i < respostasSalvas.length; i++) {
+				const respostaCorreta = data.simulado
+					.filter(
+						(pergunta) => pergunta.id === respostasSalvas[i].pergunta_id,
+					)[0]
+					.respostas.filter((respostas) => respostas.correta === true)[0];
+
+				const respostaEstaCorreta =
+					respostaCorreta.id === respostasSalvas[i].alternativa_id;
+
+				if (respostaEstaCorreta) {
+					rc += 1;
+				}
+			}
+			total = data.simulado.length;
+			re = total - rc;
+
+			const objStatus = {
+				rc,
+				re,
+				total,
+			};
+
+			console.log();
+			setStatus(objStatus);
+		}
+	}, []);
+
+	return { status };
+};
+
+export { useStatusResultado };
