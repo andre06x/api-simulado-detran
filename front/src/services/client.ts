676d85de6ffc33fdf15db7cad719365fd5e83699
andre06x <andre06x@gmail.com>
2022-12-16 23:52:06 -0300

    adicionando defaultOptions Apollo

--- front/src/services/client.ts
+++ front/src/services/client.ts
@@ -1,11 +1,28 @@
-import { ApolloClient, gql, InMemoryCache } from '@apollo/client';
+import {
+	ApolloClient,
+	DefaultOptions,
+	gql,
+	InMemoryCache,
+} from '@apollo/client';
 import { RestLink } from 'apollo-link-rest';
 
 const restLink = new RestLink({ uri: 'http://140.238.180.226:5005/' });
 
+const defaultOptions: DefaultOptions = {
+	watchQuery: {
+		fetchPolicy: 'no-cache',
+		errorPolicy: 'ignore',
+	},
+	query: {
+		fetchPolicy: 'no-cache',
+		errorPolicy: 'all',
+	},
+};
+
 const client = new ApolloClient({
 	cache: new InMemoryCache(),
 	link: restLink,
+	defaultOptions,
 });
 
 export { client };
