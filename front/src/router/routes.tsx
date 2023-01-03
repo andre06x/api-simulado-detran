0088868479f59b52082b64bc3cc2cab884f7595c
andre06x <andre06x@gmail.com>
2023-01-03 19:21:48 -0300

    att routes

--- front/src/router/routes.tsx
+++ front/src/router/routes.tsx
@@ -1,11 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from '../App';
 
 import { DadosGerais } from '../Pages/dadosGerais';
 import { Main } from '../Pages/main';
 import { NotFound } from '../Pages/notFound';
 import { Resultado } from '../Pages/resultado';
 import { Simulado } from '../Pages/simulado';
+
+const RoutesBasic = () => {
+	return (
+		<Routes>
+			<Route path="/" element={<Main />} />
+			<Route path="/simulado*" element={<Simulado />} />
+			<Route path="/resultado" element={<Resultado />} />
+
+			<Route path="/dados-gerais" element={<DadosGerais />} />
+			<Route path="*" element={<NotFound />} />
+		</Routes>
+	);
+};
 
 const RoutesApp = () => {
 	return (
@@ -22,4 +34,4 @@ const RoutesApp = () => {
 	);
 };
 
-export { RoutesApp };
+export { RoutesApp, RoutesBasic };
